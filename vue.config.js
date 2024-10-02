e51a90e02d3a7a406cd55492a7825aa8e57c83ce
Estirp3 <38378836+Estirp3@users.noreply.github.com>
2024-10-02 15:52:25 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/real-academy2/'
+    : '/'
   
   // Aquí agregamos la configuración de Webpack
   configureWebpack: {
